f86b2d1a9efb36eb3d183f3ccf01b7c67d28d1f4
razan-am <razanalamleh@gmail.com>
2021-05-02 23:38:59 +0300

    update

--- js/app.js
+++ js/app.js
@@ -6,21 +6,54 @@ let branch1 = {
     minNumOfCustPerHour: 23,
     maxNumOfCustPerHour: 65,
     AvgCookieSalePerCust: 6.3,
-    hourOperation: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 1, 2, 3, 4, 5, 6, 7],
+    numOfCustPerHour: [],
+    hourOperation: [6, 7, 8, 9, 10, 11, 12, 1, 2, 3, 4, 5, 6, 7],
+    amount: [],
+
+    randomNumber: function getRandomIntInclusive(min, max) {
+        min = Math.ceil(min);
+        max = Math.floor(max);
+        this.numOfCustPerHour = Math.floor(Math.random() * (max - min + 1) + min);
+        return (this.numOfCustPerHour)
+    },
+
+    addingNum: function () {
+        this.numOfCustPerHour.push(this.randomNumber);
+    },
+
+    calculatCookies: function () {
+        let total = this.AvgCookieSalePerCust * this.numOfCustPerHour;
+        return (total);
+
+    },
+    addCalculat: function () {
+        this.amount.push(this.calculatCookies);
+    },
+
 
     render: function () {
-        let calculat = [];
-        for (let i = 0; i < this.hourOperation.length; i++) {
-            let workHour = parseInt(prompt('please choose the work hour?'));
 
-            calculat.push = this.AvgCookieSalePerCust *= this.hourOperation[i];
-
+        for (let j = 0; j < this.hourOperation.length; j++) {
+            this.numOfCustPerHour = this.numOfCustPerHour * this.hourOperation[j];
+            document.write(this.numOfCustPerHour);
         }
-        console.log(calculat);
-        document.write(calculat[5]);
+      let divEl = document.getElementById('container');
+
+      let h2el = document.createElement ('h2');
+      h2el.textContent = this.branchName;
+      divEl.appendChild(h2el);
+
+      let ulEl = document.createElement('ul');
+      divEl.appendChild(ulEl);
+      for(let i = 0; i < this.amount.length; i++){
+          let liEl = document.createElement('li');
+          liEl.textContent=this.amount[i];
+          ulEl.appendChild(liEl)
+      }
+
     }
-
-
 }
 branch1.render();
+branch1.randomNumber(23, 65);
+branch1.addingNum();
 

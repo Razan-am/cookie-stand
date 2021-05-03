901544c314caff11f066580f2a104a00c184a296
razan-am <razanalamleh@gmail.com>
2021-05-03 13:50:13 +0300

    update lab06

--- js/app.js
+++ js/app.js
@@ -7,14 +7,29 @@ let branch1 = {
     maxNumOfCustPerHour: 65,
     AvgCookieSalePerCust: 6.3,
     hourOperation: ['6:00am', '7:00am', '8:00am', '9:00am', '10:am', '11:am', '12pm', '1:00pm', '2:00pm', '3:00pm', '4:00pm', '5:00pm', '6:00pm', '7:00pm'],
-    numOfCustPerHour: 0,
+    numOfCustPerHour: [],
+    calculatCookiesPerHour: [],
     totalAmount: 0,
 
-    randomNumber: function (min, max) {
-        min = Math.ceil(min);
-        max = Math.floor(max);
-        this.numOfCustPerHour = Math.floor(Math.random() * (max - min + 1) + min) * this.AvgCookieSalePerCust;
-        return this.numOfCustPerHour
+    randomNumber: function () {
+
+        for (let num = 0; num < this.hourOperation.length; num++) {
+            //generat random number
+            this.numOfCustPerHour.push(Math.floor(Math.random() * (this.maxNumOfCustPerHour - this.minNumOfCustPerHour + 1) + this.minNumOfCustPerHour));
+
+        }
+    },
+
+    calculation: function (){
+        for (let index = 0; index < this.hourOperation.length; index++) {
+            //calculation for avarege cookies * random number
+            this.calculatCookiesPerHour.push(Math.ceil(this.numOfCustPerHour[index]*this.AvgCookieSalePerCust));
+            
+            //total number cookies per hour
+            this.totalAmount += this.calculatCookiesPerHour[index];
+
+
+        }
     },
 
 
@@ -29,19 +44,21 @@ let branch1 = {
         divEl.appendChild(ulEl);
         for (let i = 0; i < this.hourOperation.length; i++) {
             let liEl = document.createElement('li');
-            liEl.textContent = `${this.hourOperation[i]} ${this.randomNumber(this.minNumOfCustPerHour,this.axNumOfCustPerHour)} cookies`;
             ulEl.appendChild(liEl);
-
-            this.totalAmount += this.randomNumber(this.minNumOfCustPerHour,this.axNumOfCustPerHour);
+            liEl.textContent = `${this.hourOperation[i]} ${this.calculatCookiesPerHour[i]} cookies`;
         }
 
-        liEl = document.createElement('li');
-        liEl.textContent = `Totalamount ${this.totalAmount} cookies`;
-        ulEl.appendChild(liEl);
+        let totalli = document.createElement('li');
+        ulEl.appendChild(totalli);
+        totalli.textContent = `Total: ${this.totalAmount} cookies`
 
     }
 }
+branch1.randomNumber();
+branch1.calculation();
 branch1.render();
+
+
 
 let branch2 = {
 
@@ -50,14 +67,29 @@ let branch2 = {
     maxNumOfCustPerHour: 24,
     AvgCookieSalePerCust: 1.2,
     hourOperation: ['6:00am', '7:00am', '8:00am', '9:00am', '10:am', '11:am', '12pm', '1:00pm', '2:00pm', '3:00pm', '4:00pm', '5:00pm', '6:00pm', '7:00pm'],
-    numOfCustPerHour: 0,
+    numOfCustPerHour: [],
+    calculatCookiesPerHour: [],
     totalAmount: 0,
 
-    randomNumber: function (min, max) {
-        min = Math.ceil(min);
-        max = Math.floor(max);
-        this.numOfCustPerHour = Math.floor(Math.random() * (max - min + 1) + min) * this.AvgCookieSalePerCust;
-        return this.numOfCustPerHour
+    randomNumber: function () {
+
+        for (let num = 0; num < this.hourOperation.length; num++) {
+            //generat random number
+            this.numOfCustPerHour.push(Math.floor(Math.random() * (this.maxNumOfCustPerHour - this.minNumOfCustPerHour + 1) + this.minNumOfCustPerHour));
+
+        }
+    },
+
+    calculation: function (){
+        for (let index = 0; index < this.hourOperation.length; index++) {
+            //calculation for avarege cookies * random number
+            this.calculatCookiesPerHour.push(Math.ceil(this.numOfCustPerHour[index]*this.AvgCookieSalePerCust));
+            
+            //total number cookies per hour
+            this.totalAmount += this.calculatCookiesPerHour[index];
+
+
+        }
     },
 
 
@@ -72,18 +104,18 @@ let branch2 = {
         divEl.appendChild(ulEl);
         for (let i = 0; i < this.hourOperation.length; i++) {
             let liEl = document.createElement('li');
-            liEl.textContent = `${this.hourOperation[i]} ${this.randomNumber(this.minNumOfCustPerHour,this.axNumOfCustPerHour)} cookies`;
             ulEl.appendChild(liEl);
-
-            this.totalAmount += this.randomNumber(this.minNumOfCustPerHour,this.axNumOfCustPerHour);
+            liEl.textContent = `${this.hourOperation[i]} ${this.calculatCookiesPerHour[i]} cookies`;
         }
 
-        liEl = document.createElement('li');
-        liEl.textContent = `Totalamount ${this.totalAmount} cookies`;
-        ulEl.appendChild(liEl);
+        let totalli = document.createElement('li');
+        ulEl.appendChild(totalli);
+        totalli.textContent = `Total: ${this.totalAmount} cookies`
 
     }
 }
+branch2.randomNumber();
+branch2.calculation();
 branch2.render();
 
 
@@ -94,14 +126,29 @@ let branch3 = {
     maxNumOfCustPerHour: 38,
     AvgCookieSalePerCust: 3.7,
     hourOperation: ['6:00am', '7:00am', '8:00am', '9:00am', '10:am', '11:am', '12pm', '1:00pm', '2:00pm', '3:00pm', '4:00pm', '5:00pm', '6:00pm', '7:00pm'],
-    numOfCustPerHour: 0,
+    numOfCustPerHour: [],
+    calculatCookiesPerHour: [],
     totalAmount: 0,
 
-    randomNumber: function (min, max) {
-        min = Math.ceil(min);
-        max = Math.floor(max);
-        this.numOfCustPerHour = Math.floor(Math.random() * (max - min + 1) + min) * this.AvgCookieSalePerCust;
-        return this.numOfCustPerHour
+    randomNumber: function () {
+
+        for (let num = 0; num < this.hourOperation.length; num++) {
+            //generat random number
+            this.numOfCustPerHour.push(Math.floor(Math.random() * (this.maxNumOfCustPerHour - this.minNumOfCustPerHour + 1) + this.minNumOfCustPerHour));
+
+        }
+    },
+
+    calculation: function (){
+        for (let index = 0; index < this.hourOperation.length; index++) {
+            //calculation for avarege cookies * random number
+            this.calculatCookiesPerHour.push(Math.ceil(this.numOfCustPerHour[index]*this.AvgCookieSalePerCust));
+            
+            //total number cookies per hour
+            this.totalAmount += this.calculatCookiesPerHour[index];
+
+
+        }
     },
 
 
@@ -116,18 +163,18 @@ let branch3 = {
         divEl.appendChild(ulEl);
         for (let i = 0; i < this.hourOperation.length; i++) {
             let liEl = document.createElement('li');
-            liEl.textContent = `${this.hourOperation[i]} ${this.randomNumber(this.minNumOfCustPerHour,this.axNumOfCustPerHour)} cookies`;
             ulEl.appendChild(liEl);
-
-            this.totalAmount += this.randomNumber(this.minNumOfCustPerHour,this.axNumOfCustPerHour);
+            liEl.textContent = `${this.hourOperation[i]} ${this.calculatCookiesPerHour[i]} cookies`;
         }
 
-        liEl = document.createElement('li');
-        liEl.textContent = `Totalamount ${this.totalAmount} cookies`;
-        ulEl.appendChild(liEl);
+        let totalli = document.createElement('li');
+        ulEl.appendChild(totalli);
+        totalli.textContent = `Total: ${this.totalAmount} cookies`
 
     }
 }
+branch3.randomNumber();
+branch3.calculation();
 branch3.render();
 
 let branch4 = {
@@ -137,14 +184,29 @@ let branch4 = {
     maxNumOfCustPerHour: 38,
     AvgCookieSalePerCust: 2.3,
     hourOperation: ['6:00am', '7:00am', '8:00am', '9:00am', '10:am', '11:am', '12pm', '1:00pm', '2:00pm', '3:00pm', '4:00pm', '5:00pm', '6:00pm', '7:00pm'],
-    numOfCustPerHour: 0,
+    numOfCustPerHour: [],
+    calculatCookiesPerHour: [],
     totalAmount: 0,
 
-    randomNumber: function (min, max) {
-        min = Math.ceil(min);
-        max = Math.floor(max);
-        this.numOfCustPerHour = Math.floor(Math.random() * (max - min + 1) + min) * this.AvgCookieSalePerCust;
-        return this.numOfCustPerHour
+    randomNumber: function () {
+
+        for (let num = 0; num < this.hourOperation.length; num++) {
+            //generat random number
+            this.numOfCustPerHour.push(Math.floor(Math.random() * (this.maxNumOfCustPerHour - this.minNumOfCustPerHour + 1) + this.minNumOfCustPerHour));
+
+        }
+    },
+
+    calculation: function (){
+        for (let index = 0; index < this.hourOperation.length; index++) {
+            //calculation for avarege cookies * random number
+            this.calculatCookiesPerHour.push(Math.ceil(this.numOfCustPerHour[index]*this.AvgCookieSalePerCust));
+            
+            //total number cookies per hour
+            this.totalAmount += this.calculatCookiesPerHour[index];
+
+
+        }
     },
 
 
@@ -159,20 +221,19 @@ let branch4 = {
         divEl.appendChild(ulEl);
         for (let i = 0; i < this.hourOperation.length; i++) {
             let liEl = document.createElement('li');
-            liEl.textContent = `${this.hourOperation[i]} ${this.randomNumber(this.minNumOfCustPerHour,this.axNumOfCustPerHour)} cookies`;
             ulEl.appendChild(liEl);
-
-            this.totalAmount += this.randomNumber(this.minNumOfCustPerHour,this.axNumOfCustPerHour);
+            liEl.textContent = `${this.hourOperation[i]} ${this.calculatCookiesPerHour[i]} cookies`;
         }
 
-        liEl = document.createElement('li');
-        liEl.textContent = `Totalamount ${this.totalAmount} cookies`;
-        ulEl.appendChild(liEl);
+        let totalli = document.createElement('li');
+        ulEl.appendChild(totalli);
+        totalli.textContent = `Total: ${this.totalAmount} cookies`
 
     }
 }
+branch4.randomNumber();
+branch4.calculation();
 branch4.render();
-
 
 let branch5 = {
 
@@ -181,14 +242,29 @@ let branch5 = {
     maxNumOfCustPerHour: 16,
     AvgCookieSalePerCust: 4.6,
     hourOperation: ['6:00am', '7:00am', '8:00am', '9:00am', '10:am', '11:am', '12pm', '1:00pm', '2:00pm', '3:00pm', '4:00pm', '5:00pm', '6:00pm', '7:00pm'],
-    numOfCustPerHour: 0,
+    numOfCustPerHour: [],
+    calculatCookiesPerHour: [],
     totalAmount: 0,
 
-    randomNumber: function (min, max) {
-        min = Math.ceil(min);
-        max = Math.floor(max);
-        this.numOfCustPerHour = Math.floor(Math.random() * (max - min + 1) + min) * this.AvgCookieSalePerCust;
-        return this.numOfCustPerHour
+    randomNumber: function () {
+
+        for (let num = 0; num < this.hourOperation.length; num++) {
+            //generat random number
+            this.numOfCustPerHour.push(Math.floor(Math.random() * (this.maxNumOfCustPerHour - this.minNumOfCustPerHour + 1) + this.minNumOfCustPerHour));
+
+        }
+    },
+
+    calculation: function (){
+        for (let index = 0; index < this.hourOperation.length; index++) {
+            //calculation for avarege cookies * random number
+            this.calculatCookiesPerHour.push(Math.ceil(this.numOfCustPerHour[index]*this.AvgCookieSalePerCust));
+            
+            //total number cookies per hour
+            this.totalAmount += this.calculatCookiesPerHour[index];
+
+
+        }
     },
 
 
@@ -203,17 +279,16 @@ let branch5 = {
         divEl.appendChild(ulEl);
         for (let i = 0; i < this.hourOperation.length; i++) {
             let liEl = document.createElement('li');
-            liEl.textContent = `${this.hourOperation[i]} ${this.randomNumber(this.minNumOfCustPerHour,this.axNumOfCustPerHour)} cookies`;
             ulEl.appendChild(liEl);
-
-            this.totalAmount += this.randomNumber(this.minNumOfCustPerHour,this.axNumOfCustPerHour);
+            liEl.textContent = `${this.hourOperation[i]} ${this.calculatCookiesPerHour[i]} cookies`;
         }
 
-        liEl = document.createElement('li');
-        liEl.textContent = `Totalamount ${this.totalAmount} cookies`;
-        ulEl.appendChild(liEl);
+        let totalli = document.createElement('li');
+        ulEl.appendChild(totalli);
+        totalli.textContent = `Total: ${this.totalAmount} cookies`
 
     }
 }
+branch5.randomNumber();
+branch5.calculation();
 branch5.render();
-
